bd1ae967c5891fbd0ffe314950c610ac45dc0743
chiaracarrozza <99043uk@saeinstitute.edu>
2022-06-12 23:21:51 +0100

    enemy behaviour

---
+++ Assets/Scripts/Game/Hunter/Patrol.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [Header ("Patrol Points")]
+    [SerializeField] Transform leftEdge;
+    [SerializeField] Transform rightEdge;
+
+    [Header("Enemy")]
+    [SerializeField] Transform enemy;
+
+    [Header("Movement parameters")]
+    [SerializeField] float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingLeft)
+        {
+            if(enemy.position.x <= rightEdge.position.x)
+            MoveInDirection(1);
+            else
+            {
+                DirectionChange();
+            }
+
+
+        }
+
+        else
+        {
+            if (enemy.position.x >= leftEdge.position.x)
+                MoveInDirection(-1);
+            else
+            {
+                DirectionChange();
+            }
+        }
+        MoveInDirection(1);
+    }
+
+    private void DirectionChange()
+    {
+        movingLeft=!movingLeft;
+    }
+    private void MoveInDirection(int _direction)
+    {
+        enemy.localScale= new Vector3(Mathf.Abs(initScale.x) * _direction, initScale.y, initScale.z);
+
+
+        enemy.position = new Vector3 (enemy.position.x +Time.deltaTime * _direction *speed,enemy.position.y,enemy.position.z);
+
+    }
+}
