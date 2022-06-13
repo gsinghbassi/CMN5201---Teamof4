a0118b089d78dfad28b6b21f6a6f871dc99661f6
gsinghbassi <100673uk@saeinstitute.edu>
2022-06-13 15:49:55 +0100

    fixed healthbar
    added health bar to sample scene and health bar scene

---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public int maxhealth = 100;
+    public int currenthealth;
+    public int health;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Slider slider = GetComponent<Slider>(); 
+        currenthealth = maxhealth;
+        healthBar.SetMaxHealth(maxhealth);
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TakenDamage(1);
+    }
+    
+    public void TakenDamage(int Damage)
+    {
+        currenthealth -= Damage;
+
+        healthBar.SetHealth(currenthealth);
+    }
+
+   /* private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            health -= currenthealth;
+        }
+    }*/
+}
