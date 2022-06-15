54f2b1354d687bcacc4ecd415f01090905b6ad39
chiaracarrozza <99043uk@saeinstitute.edu>
2022-06-15 18:07:31 +0100

    banjo attack

---
+++ Assets/Scripts/Game/Instruments/MusicScore.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScore : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity= transform.right *speed;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Hunter"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        if (other.gameObject.CompareTag("Walls"))
+        {
+            Destroy(gameObject);
+        }
+       
+    }
+
+
+}
+
