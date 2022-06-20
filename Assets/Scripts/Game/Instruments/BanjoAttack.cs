bd2b85e1f32b77f7eed3fe4e13aa1a979290448f
gsinghbassi <100673uk@saeinstitute.edu>
2022-06-20 17:05:08 +0100

    changes
    tried adding up and down on the create empty

--- Assets/Scripts/Game/Instruments/BanjoAttack.cs
+++ Assets/Scripts/Game/Instruments/BanjoAttack.cs
@@ -8,7 +8,7 @@ public class BanjoAttack : MonoBehaviour
     private int banjo = 0;
     private bool inventoryBanjo = false;
     [SerializeField] private Text banjotext;
-    [SerializeField] Transform attackingPointRight, attackingPointLeft;
+    [SerializeField] Transform attackingPointRight, attackingPointLeft, attackingPointup, attackingPointdown;
     [SerializeField] GameObject musicScorePrefab;
     private int counter = 0;
     [SerializeField] GameObject banjoIcon;
@@ -50,6 +50,14 @@ public class BanjoAttack : MonoBehaviour
         if (myCharacterController.myDirection == CharacterController.Direction.left)
         {
             Instantiate(musicScorePrefab, attackingPointLeft.position, attackingPointLeft.rotation);
+        }
+        if (myCharacterController.myDirection == CharacterController.Direction.up)
+        {
+            Instantiate(musicScorePrefab, attackingPointup.position, attackingPointup.rotation);
+        }
+        if (myCharacterController.myDirection == CharacterController.Direction.down)
+        {
+            Instantiate(musicScorePrefab, attackingPointdown.position, attackingPointdown.rotation);
         }
 
 
