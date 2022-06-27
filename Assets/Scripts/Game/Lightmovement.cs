f6bebc573c4ffd50172d385211c4951aac59e7f4
Indie2951 <90903457+Indie2951@users.noreply.github.com>
2022-06-27 10:58:43 +0100

    hunter light

--- Assets/Scripts/Game/Lightmovement.cs
+++ Assets/Scripts/Game/Lightmovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Lightmovement : MonoBehaviour
 {
     float hold;
+    float hold2;
     bool right;
     float pos;
     // Start is called before the first frame update
@@ -13,28 +14,31 @@ public class Lightmovement : MonoBehaviour
         hold = transform.position.x;
         if (right == true)
         {
-            transform.localPosition = new Vector3(-2.31f, -0.28f, 0);
+            transform.localPosition = new Vector3(2.32f, 0.06f, 0);
             transform.rotation = Quaternion.Euler(0, 0, 180);
         }
         else
         {
-            transform.localPosition = new Vector3(2.31f, -0.28f, 0);
-            transform.rotation = Quaternion.Euler(0,0,0);
+            transform.localPosition = new Vector3(-2.34f, -0.28f, 0);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.localPosition.x > hold)
+
+        hold = transform.localPosition.x;
+        if (transform.localPosition.x > hold2)
         {
-            transform.localPosition = new Vector3(0.53f, -0.006f, 0);
+            transform.localPosition = new Vector3(2.32f, 0.06f, 0);
             transform.rotation = Quaternion.Euler(0, 0, 180);
         }
-        else
+        if (transform.localPosition.x < hold2)
         {
-            transform.localPosition = new Vector3(-0.53f, -0.06f, 0);
+            transform.localPosition = new Vector3(-2.34f, -0.28f, 0);
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
+        hold2 = hold;
     }
 }
