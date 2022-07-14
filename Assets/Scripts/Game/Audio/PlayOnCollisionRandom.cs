b02dea7521cb48d093372f8c072dcfdbea3f5857
chiaracarrozza <99043uk@saeinstitute.edu>
2022-07-14 18:59:59 +0100

    random colliding audio

---
+++ Assets/Scripts/Game/Audio/PlayOnCollisionRandom.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnCollisionRandom: MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+    AudioClip lastClip;
+
+    AudioClip RandomClip()
+    {
+        return audioClipArray[Random.Range(0, audioClipArray.Length)];
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            audioSource.PlayOneShot(RandomClip());
+        }
+    }
+
+}
